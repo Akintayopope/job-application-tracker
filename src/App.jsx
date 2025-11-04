721936a5ff4ad554144509141153c99a1c3d6027
Akintayopope <akinpope25@gmail.com>
2025-11-03 22:34:03 -0600

    Updated index.css and deleted App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1 className='text-[10px]'>Hello</h1>
+      <h1 className='text-[100px]'>Hello</h1>
 
       <h1 class="text-5xl font-bold underline">
         Hello world!

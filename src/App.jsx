ddb6db024de9f6a90b2c8a2d56f14658f7a9e474
Akintayopope <akinpope25@gmail.com>
2025-11-03 22:19:16 -0600

    initial setup with React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-[10px]'>Hello</h1>
+
+      <h1 class="text-5xl font-bold underline">
+        Hello world!
+      </h1> </div>
+  )
+}
+
+export default App
